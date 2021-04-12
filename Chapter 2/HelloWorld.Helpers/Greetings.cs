331064638be5cdae3b7593e4fed7dab421aafdf2
Marcel Wagner <marcel.alex.wagner@outlook.com>
2021-04-12 19:21:54 +0200

    Add chapter 2 source code

---
+++ Chapter 2/HelloWorld.Helpers/Greetings.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld.Helpers
+{
+    public class Greetings
+    {
+        public static string GetStandardGreeting()
+        {
+            return "Hello from a cross platform library!";
+        }
+    }
+}
