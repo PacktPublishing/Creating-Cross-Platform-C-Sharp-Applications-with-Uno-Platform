a8ecb556aa5d30c29a334a8ba991274faa01647b
Matt Lacey <matt@mrlacey.co.uk>
2021-04-26 10:34:49 +0100

    Add WebAPI code (as used by CH4)

---
+++ WebApi/UnoBookRail.WebApi/Controllers/QuickReportsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace UnoBookRail.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuickReportsController : ControllerBase
+    {
+        public QuickReportsController()
+        {
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<string>> CreateReport([FromForm] IFormFile imageFile, [FromForm] string location, [FromForm] string information)
+        {
+            // A real app would do something with the provided data
+            System.Diagnostics.Debug.WriteLine($"{imageFile} {location} {information}");
+
+            return await Task.FromResult<ActionResult<string>>("success");
+        }
+    }
+}
---
+++ WebApi/UnoBookRail.WebApi/Controllers/StationsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnoBookRail.Common.Network;
+
+namespace UnoBookRail.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StationsController : ControllerBase
+    {
+        public StationsController()
+        {
+        }
+
+        [HttpGet]
+        public Arrivals GetNextArrivals(int stationId)
+        {
+            return new Stations().GetNextArrivals(stationId);
+        }
+    }
+}
